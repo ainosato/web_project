28af05aab440f89ee3c181c1c913c1bdd120af17
ainosato <56681766+ainosato@users.noreply.github.com>
2020-05-16 01:41:19 +0900

    php practice

---
+++ number.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Number & Operator </h1>
+    <h1>1+1</h1>
+    <?php
+    echo 1 + 1;
+     ?>
+     <h1>2-1</h1>
+     <?php
+     echo 2 - 1;
+      ?>
+      <h1>2*2</h1>
+      <?php
+      echo 4/2;
+       ?>
+
+  </body>
+</html>
---
+++ variable.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "leezche";
+    echo "Lorem ipsum ".$name." dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, ".$name." quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo ainosato consequat. Duis aute irure dolor
+    in reprehenderit in voluptate velit esse cillum dolore eu
+    fugiat nulla pariatur. Excepteur ainosato sint occaecat cupidatat non proident,
+    sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ?>
+
+  </body>
+</html>
