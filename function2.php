2964bf4a5495d1692b009b3ddcb78b375d2a1b0a
ainosato <56681766+ainosato@users.noreply.github.com>
2020-05-19 01:59:37 +0900

    php practice

---
+++ function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+      function basic() {
+        print("loreasdf sdf<br>");
+        print("loreasdf sdf");
+
+      }
+
+      basic();
+     ?>
+
+  </body>
+</html>
