7c024305958bd7ef7ae8bc54695b152990b32521
ainosato <56681766+ainosato@users.noreply.github.com>
2020-05-19 01:59:03 +0900

    second version

--- lol.php
+++ lol.php
@@ -1,52 +1,57 @@
+<?php
+  function print_title() {
+    if (isset($_GET['id'])) {
+      echo $_GET['id'];
+    }
+    else {
+      echo "Welcome";
+    }
+  }
+  function print_list() {
+    $list = scandir('./data');
+    $i = 0;
+    while ($i < count($list)) {
+      if ($list[$i] != '.') {
+        if ($list[$i] != '..') {
+          echo "<li><a href=\"lol.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        }
+      }
+      $i++;
+    }
+  }
+  function print_description() {
+    if (isset($_GET['id'])) {
+      echo file_get_contents("data/".$_GET['id']);
+    }
+    else {
+      echo "Hello, 소환사!";
+    }
+  }
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+    <?php
+      print_title();
+    ?>
+    </title>
   </head>
   <body>
     <h1><a href="lol.php">League of Legend</a></h1>
     <ol>
       <?php
-        $list = scandir('./data');
-
-        $i = 0;
-        while ($i < count($list)) {
-          if ($list[$i] != '.') {
-            if ($list[$i] != '..') {
-              echo "<li><a href=\"lol.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-
-          }
-
-          $i++;
-        }
+        print_list();
        ?>
     </ol>
-
-
     <h2>
       <?php
-
-          if (isset($_GET['id'])) {
-            echo $_GET['id'];
-          }
-          else {
-            echo "Welcome";
-          }
-
+        print_title();
       ?>
     </h2>
       <?php
-        if (isset($_GET['id'])) {
-          echo file_get_contents("data/".$_GET['id']);
-        }
-        else {
-          echo "Hello, 소환사!";
-        }
-
-       ?>
-
-
-  </body>
+        print_description();
+      ?>
+      </body>
 </html>
