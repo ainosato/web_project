b8851fe9580b39db0df5da600a9693392bec539c
ainosato <56681766+ainosato@users.noreply.github.com>
2020-05-24 18:28:08 +0900

    php practice

---
+++ comparison.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Comparison Operatorws &amp; Boolean data type</h1>
+    <h2>1==1</h2>
+    <?php
+      var_dump(1==1);
+     ?>
+     <h2>1>1</h2>
+     <?php
+       var_dump(1>1);
+      ?>
+  </body>
+</html>
